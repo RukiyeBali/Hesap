098b2a4b25fd2bee1b89cf12a74e51beb499cdbb
RukiyeBali <145977861+RukiyeBali@users.noreply.github.com>
2023-09-26 15:00:34 +0300

    Add files via upload

---
+++ Basit-hesap-makinesi/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Basit_hesap_makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.btnCıkar = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.lblSayi1 = new System.Windows.Forms.Label();
+            this.lblSayı2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.BackColor = System.Drawing.Color.Red;
+            this.btnTopla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTopla.Location = new System.Drawing.Point(217, 12);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(99, 32);
+            this.btnTopla.TabIndex = 0;
+            this.btnTopla.Text = "Topla";
+            this.btnTopla.UseVisualStyleBackColor = false;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // btnCıkar
+            // 
+            this.btnCıkar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnCıkar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCıkar.Location = new System.Drawing.Point(322, 12);
+            this.btnCıkar.Name = "btnCıkar";
+            this.btnCıkar.Size = new System.Drawing.Size(99, 32);
+            this.btnCıkar.TabIndex = 1;
+            this.btnCıkar.Text = "Çıkar";
+            this.btnCıkar.UseVisualStyleBackColor = false;
+            this.btnCıkar.Click += new System.EventHandler(this.btnCıkar_Click);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.BackColor = System.Drawing.Color.Gold;
+            this.btnCarp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCarp.Location = new System.Drawing.Point(217, 50);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(99, 32);
+            this.btnCarp.TabIndex = 2;
+            this.btnCarp.Text = "Çarp";
+            this.btnCarp.UseVisualStyleBackColor = false;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.BackColor = System.Drawing.Color.Lime;
+            this.btnBol.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBol.Location = new System.Drawing.Point(322, 50);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(99, 32);
+            this.btnBol.TabIndex = 3;
+            this.btnBol.Text = "Böl";
+            this.btnBol.UseVisualStyleBackColor = false;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // lblSayi1
+            // 
+            this.lblSayi1.AutoSize = true;
+            this.lblSayi1.BackColor = System.Drawing.Color.Transparent;
+            this.lblSayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayi1.Location = new System.Drawing.Point(27, 22);
+            this.lblSayi1.Name = "lblSayi1";
+            this.lblSayi1.Size = new System.Drawing.Size(42, 15);
+            this.lblSayi1.TabIndex = 4;
+            this.lblSayi1.Text = "Sayı1";
+            // 
+            // lblSayı2
+            // 
+            this.lblSayı2.AutoSize = true;
+            this.lblSayı2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayı2.Location = new System.Drawing.Point(27, 60);
+            this.lblSayı2.Name = "lblSayı2";
+            this.lblSayı2.Size = new System.Drawing.Size(42, 15);
+            this.lblSayı2.TabIndex = 5;
+            this.lblSayı2.Text = "Sayı2";
+            this.lblSayı2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(65, 153);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(47, 15);
+            this.lblSonuc.TabIndex = 6;
+            this.lblSonuc.Text = "Sonuç";
+            this.lblSonuc.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(68, 57);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 7;
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(68, 19);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(433, 218);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lblSayı2);
+            this.Controls.Add(this.lblSayi1);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btnCıkar);
+            this.Controls.Add(this.btnTopla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Button btnCıkar;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.Label lblSayi1;
+        private System.Windows.Forms.Label lblSayı2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.TextBox txtSayi1;
+    }
+}
+
---
+++ Basit-hesap-makinesi/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basit_hesap_makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToInt32(txtSayi1.Text);
+            sayi2 = Convert.ToInt32(txtSayi2.Text);
+            sonuc = sayi1 + sayi2;
+
+            lblSonuc.Text= "sonuc: "+sonuc.ToString();
+
+        }
+
+        private void btnCıkar_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToInt32(txtSayi1.Text);
+            sayi2 = Convert.ToInt32(txtSayi2.Text);
+            sonuc = sayi1 - sayi2;
+
+            lblSonuc.Text = "Fark: "+ sonuc.ToString();
+        }
+
+        private void btnCarp_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToInt32(txtSayi1.Text);
+            sayi2 = Convert.ToInt32(txtSayi2.Text);
+            sonuc = sayi1 * sayi2;
+
+            lblSonuc.Text = "sonuc: " + sonuc.ToString();
+        }
+
+        private void btnBol_Click(object sender, EventArgs e)
+        {
+            float sayi1, sayi2, sonuc;
+            sayi1 = Convert.ToInt32(txtSayi1.Text);
+            sayi2 = Convert.ToInt32(txtSayi2.Text);
+            sonuc = sayi1 / sayi2;
+
+            lblSonuc.Text = "sonuc: " + sonuc.ToString();
+        }
+    }
+}
